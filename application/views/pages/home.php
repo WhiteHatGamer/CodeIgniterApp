f22b22ebed713443d7ee2485796234c34655affe
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-01-19 20:01:07 +0530

    homePageView

---
+++ application/views/pages/home.php
@@ -0,0 +1,4 @@
+<h3><a href="<?=$_SERVER['PHP_SELF']?>/news">News</a></h3>
+<h3><a href="<?=$_SERVER['PHP_SELF']?>/admin/">Dashboard</a></h3>
+<h3><a href="<?=$_SERVER['PHP_SELF']?>/admin/Travel_planner">Travel Planner</a></h3>
+<h3><a href="<?=$_SERVER['PHP_SELF']?>/About">About Us</a></h3>
