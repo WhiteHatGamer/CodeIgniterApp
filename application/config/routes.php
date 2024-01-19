87906438e1dd4d6c65a1a3f5e3d7b2b4c94a1cf5
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-01-19 19:59:58 +0530

    homePageRoute

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,3 +54,5 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['news'] = 'news';
 $route['news/(:any)'] = 'news/view/$1';
+// $route['admin/(:any)'] = 'admin/pages/$1';
+$route['(:any)'] = 'pages/view/$1';
