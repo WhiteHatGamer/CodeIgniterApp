cc29f74d43d6223be47e42ff57351fc05224c98c
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-01-19 16:00:25 +0530

    databaseUpdate

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+// TODO: Set Your own Database Details
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
+	'hostname' => 'localhost:3307',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'form',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
