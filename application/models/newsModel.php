9841f82892a738d044ab2d5eadcaa6e00dc32075
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-01-19 20:05:40 +0530

    getNewsFunctionDefinitionModel

--- application/models/newsModel.php
+++ application/models/newsModel.php
@@ -7,4 +7,13 @@
             $this->load->database();
         }
 
+        public function getNews($slug = false){
+            if($slug === FALSE){
+                $query = $this->db->get('news');
+                return $query->result_array();
+            }
+            
+            $query = $this->db->get_where('news', array('slug' => htmlspecialchars($slug)));
+            return $query->result_array();
+        }
     }
