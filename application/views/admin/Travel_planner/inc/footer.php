4db196927bb6fe76920967ef5f78913720b33724
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-01-20 23:22:56 +0530

    travel_planner_views_header_footer_template

---
+++ application/views/admin/Travel_planner/inc/footer.php
@@ -0,0 +1,45 @@
+    <!-- jQuery -->
+    <script src="<?=base_url('assets/plugins/jquery/jquery.min.js')?>"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?=base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+    <!-- SweetAlert2 -->
+    <script src="<?=base_url('assets/plugins/sweetalert2/sweetalert2.min.js')?>"></script>
+    <!-- AdminLTE App -->
+    <script src="<?=base_url('assets/dist/js/adminlte.min.js')?>"></script>
+    <!-- DataTables  & Plugins -->
+    <script src="<?=base_url('assets/plugins/datatables/jquery.dataTables.min.js')?>"></script>
+    <script src="<?=base_url('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')?>"></script>
+    <script src="<?=base_url('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')?>"></script>
+    <script src="<?=base_url('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')?>"></script>
+    <script src="<?=base_url('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js')?>"></script>
+    <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')?>"></script>
+    <script src="<?=base_url('assets/plugins/jszip/jszip.min.js')?>"></script>
+    <script src="<?=base_url('assets/plugins/pdfmake/pdfmake.min.js')?>"></script>
+    <script src="<?=base_url('assets/plugins/pdfmake/vfs_fonts.js')?>"></script>
+    <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.html5.min.js')?>"></script>
+    <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.print.min.js')?>"></script>
+    <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.colVis.min.js')?>"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="<?=base_url('assets/dist/js/demo.js')?>"></script>
+    <div style="background-image: url(
+                                    'https://static.vecteezy.com/system/resources/previews/016/005/875/original/arrow-road-yellow-sign-warning-striped-arrow-safety-type-construction-border-isolated-on-white-background-illustration-vector.jpg'
+                                    ); background-position: center; background-size: 111%;">
+    <footer>
+        <strong style="-webkit-text-stroke: 0.5px #fad320; color: #020202; font-size: larger;">
+            TravelPlanner&copy; 2023
+        </strong>
+        <?php
+
+          function calculate_file_path(){
+            $views = 'C:\xampp\htdocs\CodeIgniterApp\application\views\\';
+            $add_string = '/index.php';
+            $url = $_SERVER['PHP_SELF'];
+            $url = str_replace('User_profile','Dashboard/Profile',$url);
+            $url = str_replace('hotel','Dashboard/ListStays',$url);
+            $url = str_replace('/CodeIgniterApp/index.php/',$views,$url).$add_string;
+            echo $url;
+          }
+        ?>
+    </footer>
+    </div>
+    
---
+++ application/views/admin/Travel_planner/inc/header.php
@@ -0,0 +1,32 @@
+<div style="background-image: url('https://static.vecteezy.com/system/resources/previews/026/730/373/non_2x/black-and-yellow-warning-line-striped-sign-background-eps10-vector.jpg'); background-position: center;">
+<!-- https://static.vecteezy.com/system/resources/previews/026/730/373/non_2x/black-and-yellow-warning-line-striped-sign-background-eps10-vector.jpg -->
+<!-- https://c8.alamy.com/comp/2FKYC98/top-view-of-dark-wet-asphalt-road-with-three-yellow-arrow-signs-high-resolution-full-frame-textured-background-of-black-asphalt-viewed-from-above-2FKYC98.jpg -->
+<!-- https://cdn2.vectorstock.com/i/1000x1000/36/31/black-and-yellow-chevron-stylish-texture-vector-25153631.jpg -->
+
+  <header class="main-header brand-link" style="margin-left: 0% !important;">
+    
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?=base_url('assets/plugins/fontawesome-free/css/all.min.css')?>">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="<?=base_url('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')?>">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?=base_url('assets/dist/css/adminlte.min.css')?>">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="<?=base_url('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')?>">
+    <link rel="stylesheet" href="<?=base_url('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')?>">
+    <link rel="stylesheet" href="<?=base_url('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')?>">
+    <!-- SweetAlert2 -->
+    <link rel="stylesheet" href="<?=base_url('assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')?>">
+    <!-- Toastr -->
+    <link rel="stylesheet" href="<?=base_url('assets/plugins/toastr/toastr.min.css')?>">
+    <!-- jQuery -->
+    <script src="<?=base_url('assets/plugins/jquery/jquery.min.js')?>"></script> 
+    
+    <a href="<?=adminTravelPlannerUrl()?>">
+      <img src="<?=base_url("assets/dist/img/Travel_planner_128.png")?>" alt="Travel Planner Logo" class="brand-image img-circle img-sm elevation-2" style="opacity: .8">
+      <span class="brand-text"><b>T</b>ravel <b>P</b>lanner</span>
+    </a>
+  </header>
+</div>
