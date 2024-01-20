2bb7a05d6e94d03e6499ff89d00c8724dff92da9
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-01-20 22:31:46 +0530

    travelPlannerIncludeHelperFunctionDefined

--- application/helpers/asset_helper.php
+++ application/helpers/asset_helper.php
@@ -4,4 +4,9 @@
     function assetUrl(){
         return base_url().'assets/';
     }
+
+    // Helper to get assets folder from Base folder
+    function Travel_plannerInc(){
+        return APPPATH.'views/admin/Travel_planner/inc/';
+    }
 ?>
