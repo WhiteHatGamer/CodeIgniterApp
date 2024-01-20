9eb4413b6340726d639d727445d65db3cadf203b
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-01-20 23:02:46 +0530

    admin_view_controller_examples_method_defined

--- application/controllers/admin/views.php
+++ application/controllers/admin/views.php
@@ -128,5 +128,25 @@
             $this->load->view('admin/views/mailbox/'.$page);
         
         }
+
+        public function examples($page){
+                        
+            // Checking if Empty argument is passed
+            if(empty($page)){
+
+                show_404();
+            }
+            
+            // Checking if file Available
+            $requestedFile = APPPATH.'views/admin/views/examples/'.$page;
+            if(!file_exists($requestedFile)){
+                
+                // Page doesn't Exists
+                show_404();
+            }
+            
+            $this->load->view('admin/views/examples/'.$page);
+        
+        }
     }
 ?>
