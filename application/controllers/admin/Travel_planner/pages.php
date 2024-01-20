cc2980d6c9e8fcd8d031c088e815d79504e8cfa7
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-01-20 23:13:00 +0530

    TravelPlannerController_indexMethod_With_validation

--- application/controllers/admin/Travel_planner/pages.php
+++ application/controllers/admin/Travel_planner/pages.php
@@ -11,5 +11,23 @@
             $this->load->model('user_model');
         }
 
+        public function index(){
+
+            @session_start();
+
+            if((!$this->session->email) && ($this->session->remail)){
+                $data= $this->user_model->get_user_details($this->session->remail);
+                $this->load->view('admin/Travel_planner/inc/logged_out',$data);
+                return;
+            }
+            if($this->session->email){
+                redirect(adminTravelPlannerUrl()."dashboard");
+            }
+
+            $this->load->view('admin/Travel_planner/inc/header');
+            $this->load->view('admin/Travel_planner/index');
+            $this->load->view('admin/Travel_planner/inc/footer');
+        }
+        
     }
 ?>
