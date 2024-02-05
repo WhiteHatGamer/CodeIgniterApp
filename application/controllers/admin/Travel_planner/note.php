2286bcc8e7945eb116159758df2050c3425ffca3
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-05 18:36:58 +0530

    note_list_function_index

--- application/controllers/admin/Travel_planner/note.php
+++ application/controllers/admin/Travel_planner/note.php
@@ -15,6 +15,28 @@
             $this->load->library('session');
         }
 
+        public function index(){
+            // Checking If User is Logged In
+            if(!$this->session->email){
+              redirect(adminTravelPlannerUrl());
+                return;
+            }
+            
+            $result = $this->note_model->get_notes();
+            if(!$result){
+                // No Data Stored
+                $this->load->view("admin\Travel_planner\inc/header");
+                $this->load->view("admin\Travel_planner\inc/no_data_note");
+                $this->load->view("admin\Travel_planner\inc/footer");
+            }else{
+                
+                $data['result']=$result;
+                // Details Available
+                $this->load->view("admin\Travel_planner\inc/header");
+                $this->load->view("admin\Travel_planner\Dashboard\ReadNote\index",$data);
+                $this->load->view("admin\Travel_planner\inc/footer");
+            }
+        }
     }
 
 ?>
