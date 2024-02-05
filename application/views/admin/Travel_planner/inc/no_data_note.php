9fe7b2a20e83bf4d00715b84b4b0c4c75c705fab
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-05 18:37:45 +0530

    no_note_view

---
+++ application/views/admin/Travel_planner/inc/no_data_note.php
@@ -0,0 +1,7 @@
+<div style="min-height: 89%;">
+    <div class="callout callout-danger">
+        <h5><i class="icon fas fa-ban"></i> NO DATA!!</h5>
+        Sorry!! No Note is Stored in Database <br>Please Add Notes Using This <b><a href='<?=adminTravelPlannerUrl()?>note/create'>Link</a></b>
+
+    </div>
+</div>
