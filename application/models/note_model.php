4ccc099ad0ae30487ee0af95f2415c62decd91d7
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-05 18:42:04 +0530

    get_notes_method

--- application/models/note_model.php
+++ application/models/note_model.php
@@ -15,5 +15,23 @@
             $this->load->library('session');
         }
 
+        public function get_notes(){
+            $email  = $this->session->email;
+
+            $result = $this->db->get_where($this->table,array('email'=>$email));
+
+            if($result->num_rows() > 0){
+
+                $notes = array();
+                foreach ($result->result_array() as $key => $value){
+                    array_push($notes,$value);
+                }
+        
+                return $notes;
+            }else{
+                return false;
+            }
+
+        }
     }
 ?>
