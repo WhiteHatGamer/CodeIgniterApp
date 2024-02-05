0f165bbffe6db319b9470a214640a652cc450bdc
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-05 17:03:08 +0530

    confirm_password_ajax

--- application/views/admin/Travel_planner/Register/index.php
+++ application/views/admin/Travel_planner/Register/index.php
@@ -101,4 +101,16 @@
     </div>
     <!-- /.register-page -->
   </body>
+  <script>
+      var password = document.getElementById("password");
+      var confirm_password = document.getElementById("confirm_password");
+
+      function validatePassword(){
+        if(password.value != confirm_password.value) {
+          confirm_password.setCustomValidity("Passwords Don't Match");
+        } else {
+          confirm_password.setCustomValidity('');
+        }
+      }
+  </script>
 </html>
