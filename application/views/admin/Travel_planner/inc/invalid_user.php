fe6608af919efac8b9eb95de58a315ea7a0b60ec
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-05 14:44:28 +0530

    invalid_user_toast

---
+++ application/views/admin/Travel_planner/inc/invalid_user.php
@@ -0,0 +1,23 @@
+
+<!-- SweetAlert2 -->
+<link rel="stylesheet" href="<?=base_url('assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')?>">
+<!-- jQuery -->
+    <script src="<?=base_url('assets/plugins/jquery/jquery.min.js')?>"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?=base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+    <!-- SweetAlert2 -->
+    <script src="<?=base_url('assets/plugins/sweetalert2/sweetalert2.min.js')?>"></script>
+<script>
+$(function() {
+var Toast = Swal.mixin({
+    toast: false,
+    showConfirmButton: true,
+    timer: 0
+});
+
+Toast.fire({
+    icon: 'error',
+    title: 'Login Failed!!\nRetry Again.'
+    })
+});
+</script>
