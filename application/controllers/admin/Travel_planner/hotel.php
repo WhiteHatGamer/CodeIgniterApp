e267e37f63763d11d3164d94398c3dccec836400
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-07 14:45:26 +0530

    index_page_with_post_operation_actions

--- application/controllers/admin/Travel_planner/hotel.php
+++ application/controllers/admin/Travel_planner/hotel.php
@@ -16,4 +16,71 @@
             $this->load->library(array('session','table'));
         }
 
+        public function index(){
+
+            // Checking If User is Logged In
+            if(!$this->session->email){
+                redirect(adminTravelPlannerUrl());
+                return;
+            }
+
+            if(isset($_POST['edit'])){
+                
+                $edit = $this->hotel_model->get_stay($this->input->post('edit'));
+                if(!$edit){
+
+                    redirect(adminTravelPlannerUrl().'hotel/');
+                }else{
+                    
+                    $data['edit'] = $edit;
+                }
+            }
+
+            if($this->input->post('stay')){
+                if($this->hotel_model->update_stay($this->input->post('stay'))){
+
+                    $this->load->view("admin\Travel_planner\inc\saved");
+                }else{
+                    $data['errorTitle'] = "Not Saved";
+                    $data['error'] = "Unable To Update Details";
+                    $this->load->view("admin\Travel_planner\inc\warning",$data);
+                }
+
+            }
+
+            if($this->input->post('delete')){
+                $delete = $this->hotel_model->get_stay($this->input->post('delete'));
+                if(!$delete){
+
+                    redirect(adminTravelPlannerUrl().'hotel/');
+                }else{
+                    
+                    $data['delete'] = $delete;
+                }
+            }
+            
+            if($this->input->post('confirm_delete')){
+                if($this->hotel_model->delete_stay($this->input->post('confirm_delete'))){
+                    $this->load->view("admin\Travel_planner\inc\deleted");
+                }else{
+                    
+                    redirect(adminTravelPlannerUrl().'hotel/');
+                }
+            }
+
+            $result = $this->hotel_model->get_stays();
+            if(!$result){
+                // No Data Stored
+                $this->load->view("admin\Travel_planner\inc/header");
+                $this->load->view("admin\Travel_planner\inc/no_data_hotel");
+                $this->load->view("admin\Travel_planner\inc/footer");
+            }else{
+                
+                $data['result']=$result;
+                // Details Available
+                $this->load->view("admin\Travel_planner\inc/header");
+                $this->load->view("admin\Travel_planner\Dashboard\ListStays\index",$data);
+                $this->load->view("admin\Travel_planner\inc/footer");
+            }
+        }
     }
