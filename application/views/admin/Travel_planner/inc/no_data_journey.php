f34bbf56b4bd5337ac61145701e0671b9a64caa7
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-19 19:42:59 +0530

    no_journey_in_db_inc

---
+++ application/views/admin/Travel_planner/inc/no_data_journey.php
@@ -0,0 +1,7 @@
+<div style="min-height: 89%;">
+    <div class="callout callout-danger">
+        <h5><i class="icon fas fa-ban"></i> NO DATA!!</h5>
+        Sorry!! No Trip is in Database <br>Please Add journey Using This <b><a href='<?=adminTravelPlannerUrl()?>journey/plan'>Link</a></b>
+
+    </div>
+</div>
