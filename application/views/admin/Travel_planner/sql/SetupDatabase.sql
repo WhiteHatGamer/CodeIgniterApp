703737df790a5d74621ad3e5af81c832a9acb5fe
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-19 19:43:40 +0530

    create_user_table

--- application/views/admin/Travel_planner/sql/SetupDatabase.sql
+++ application/views/admin/Travel_planner/sql/SetupDatabase.sql
@@ -5,3 +5,16 @@
 CREATE DATABASE IF NOT EXISTS `form`;
 USE `form`;
 
+-- Create User Table
+CREATE TABLE user(
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(260) NOT NULL,
+    `name` VARCHAR(100),
+    `number` INT,
+    `dob` DATE,
+    `image` BLOB,
+    `gender` enum('m','f','o'),
+    `prefix` enum('Mr.','Ms.','Mrs.','Dr.','Adv.'),
+    PRIMARY KEY(`email`)
+)
+
