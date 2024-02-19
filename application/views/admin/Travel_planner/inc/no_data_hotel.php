c683506928e36d01f9cf9d7b2286666ab9558c20
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-19 19:42:50 +0530

    no_data_hotel_inc

---
+++ application/views/admin/Travel_planner/inc/no_data_hotel.php
@@ -0,0 +1,7 @@
+<div style="min-height: 89%;">
+    <div class="callout callout-danger">
+        <h5><i class="icon fas fa-ban"></i> NO DATA!!</h5>
+        Sorry!! No Hotel Stay is Stored in Database <br>Please Add Stay Using This <b><a href='<?=adminTravelPlannerUrl()?>hotel/appoint'>Link</a></b>
+
+    </div>
+</div>
