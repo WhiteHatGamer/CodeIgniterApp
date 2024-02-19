26ca9bdaf096319ea95aafd7e7a32a3dc0df3cbd
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-19 19:35:56 +0530

    password_update_view

---
+++ application/views/admin/Travel_planner/Dashboard/Profile/Password.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Password</title>
+</head>
+<body>
+    <div class="center-page" style="background-image: url(
+        https://media.wallpaperchill.com/lock-screen-wallpapers/lock-screen-background-hd-wallpaper-s105.jpg
+    ); background-position: center;">
+        <div class="card" style="background-color: rgba(242, 242, 242, 0.7);">
+            <div class="card-header">
+                <h3 class="card-title"><b>C</b>hange <b>P</b>assword</h3>
+            </div>
+            <div class="card-body">
+                <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+                    <input style="background-color: rgba(242, 242, 242, 0.7);" class="form-control" type="password" placeholder='Old Password' name="OldPassword" required><br>
+                    <input style="background-color: rgba(242, 242, 242, 0.7);" class='form-control' type="password" placeholder="New Password" name="NewPassword" id="NewPassword" required onchange=validatePassword()>
+                    <div class="input-group mb-3">
+                        <input style="background-color: rgba(242, 242, 242, 0.7);" class='form-control' type="password" placeholder="Confirm Password" name="ConfirmPassword" id="ConfirmPassword" required onkeyup=validatePassword()>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                            <button type="button" name="hidden" onclick="
+                                                                        if(this.name=='hidden'){
+                                                                            ConfirmPassword.type='text';
+                                                                            this.name='shown';
+                                                                            this.className='btn btn-xs fa fa-eye';
+                                                                        }
+                                                                        else{
+                                                                            ConfirmPassword.type='password';
+                                                                            this.name='hidden';
+                                                                            this.className='btn btn-xs fa fa-eye-slash';
+                                                                        }"
+                                                                        class="btn btn-xs fa fa-eye-slash"></button>
+                            </div>
+                            <br>
+                        </div>
+                    </div>
+                    <button class="btn bg-gradient-danger btn-xs float-right" name="change" type="submit">Change Password</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <a class='float-right' href='vscode://file/C:\xampp\htdocs\CodeIgniterApp\application\views\admin\Travel_planner\Dashboard\Profile\Password.php'><button class='btn btn-sm btn-dark'>Open In</button><button class="btn btn-sm btn-outline-primary">Code</button></a>
+</body>
+
+
+</html>
