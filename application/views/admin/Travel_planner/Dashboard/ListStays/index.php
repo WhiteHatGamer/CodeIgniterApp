867904033ae8c1adb4b041db624519aa341de004
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-19 19:50:36 +0530

    delete_confirmation_views

--- application/views/admin/Travel_planner/Dashboard/ListStays/index.php
+++ application/views/admin/Travel_planner/Dashboard/ListStays/index.php
@@ -104,3 +104,116 @@
             </div>
         <?php
         }
+
+        // Delete Confirmation
+        if (isset($_POST['delete'])) { ?>
+            <div class="card-body" style="background-color: rgba(255, 255, 255, .5);">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title"><b>C</b>onfirm <b>D</b>elete</h3>
+                    </div>
+                    <div class="card-body p-0" style="background-color: rgba(255, 255, 255, .75);">
+
+                        <form method='post' action='#'>
+                            <table class='table table-bordered table-striped text-center'>
+                                <tr>
+                                    <th>City</th>
+                                    <th>Hotel</th>
+                                    <th>CheckIn</th>
+                                    <th>CheckOut</th>
+                                    <th>Confirm</th>
+                                    <th>Cancel</th>
+                                </tr>
+
+                            <?php
+                            echo "<tr>";
+                            foreach ($delete as $key => $value) {
+                                if ($key == "id") {
+                                    $id = $value;
+                                    continue;
+                                } elseif ($key == 'email') {
+                                    continue;
+                                }
+                                if ($key == 'checkIn' || $key == 'checkOut') {
+                                    echo "<td>" . date('d-M-Y', strtotime($value)) . "</td>";
+                                } else {
+                                    echo "<td>" . $value . "</td>";
+                                }
+                            }
+                            // Button with id as Value
+                            echo "<td><button class='btn bg-gradient-danger' type='submit' name='confirm_delete' value={$_POST['delete']}><i class='fas fa-check'></i></button></td>";
+                            echo "<td><button class='btn bg-gradient-info' type='submit' name='cancel'><i class='fas fa-times'></i></button></td>";
+                            echo "</tr>";
+                            echo "</table></form></div></div></div>";
+        }
+
+
+        ?>
+        <div style="min-height: 89%; background-image: url(
+                        'https://static.vecteezy.com/system/resources/previews/016/257/593/original/hotel-icon-set-room-guest-reception-bed-breakfast-hospitality-concierge-inn-line-sign-symbol-simple-outline-style-illustration-design-isolated-on-white-background-eps-10-vector.jpg'
+                        ); background-position: center;">
+            <div class="card" style="background-color: rgba(255, 255, 255, .5);">
+                <div class="card-header">
+                    <h3 class="card-title"><b>H</b>otel <b>S</b>tays</h3>
+                    <div class="btn-group float-right">
+                        <button type="button" class="btn btn-outline-success dropdown-toggle btn-sm" data-toggle="dropdown"><i class="fas fa-sign-out-alt"></i>Export<span class="sr-only"></span></button>
+                        <div class="dropdown-menu" role="menu">
+                            <button onclick="export_table('excel')" class="dropdown-item btn-sm"><i class="fas fa-file-excel"></i>Excel</button>
+                            <button onclick="export_table('pdf')" class="dropdown-item btn-sm"><i class="fas fa-file-pdf"></i>PDF</button>
+                            <button onclick="export_table('csv')" class="dropdown-item btn-sm"><i class="fas fa-file-csv"></i>CSV</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body" style="background-color: rgba(255, 255, 255, .75);">
+                    <div id="hotel_table_wrapper" class="dataTables_wrapper dt-bootstrap4">
+
+                        <form method='post' action="<?= $_SERVER['PHP_SELF'] ?>">
+                            <table id="hotel_table" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>City</th>
+                                        <th>Hotel</th>
+                                        <th>CheckIn</th>
+                                        <th>CheckOut</th>
+                                        <th>Edit</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                </thead>
+
+                                <?php
+                                foreach ($result as $row) {
+                                    echo "<tr>";
+                                    foreach ($row as $key => $value) {
+                                        if ($key == "id") {
+                                            $id = $value;
+                                            continue;
+                                        } elseif ($key == 'email') {
+                                            continue;
+                                        }
+                                        if ($key == 'checkIn' || $key == 'checkOut') {
+                                            echo "<td>" . date('d-M-Y', strtotime($value)) . "</td>";
+                                        } else {
+                                            echo "<td>" . $value . "</td>";
+                                        }
+                                    }
+                                    echo "<td><button class='btn bg-gradient-warning' type='submit' name='edit' value=$id><i class='fas fa-tools'></i></button></td>";
+                                    echo "<td><button class='btn bg-gradient-danger'  type='submit' name='delete' value=$id><i class='fas fa-skull-crossbones'></i></button></td>";
+                                    echo "</tr>";
+                                }
+                                echo '</table> </form> </div> </div> </div>'
+                                ?>
+        </div>
+        <a class='float-right' href='vscode://file/C:\xampp\htdocs\CodeIgniterApp\application\views\admin\Travel_planner\Dashboard\ListStays\index.php'><button class='btn btn-sm btn-dark'>Open In</button><button class="btn btn-sm btn-outline-primary">Code</button></a>
+
+        <!-- Page specific script -->
+        <?php
+        if (isset($_POST['edit'])) { ?>
+            <script>
+                // Calculating Expense Of the stay Editing
+                calculateExpense("<?= date('Y-m-d', strtotime($edit['checkOut'])); ?>");
+            </script>
+        <?php } ?>
+
+    </body>
+
+</html>
