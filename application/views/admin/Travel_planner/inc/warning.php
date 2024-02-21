4571caa886a4e6a1f9d985e8d7b694b20da86438
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-02-21 17:48:28 +0530

    todo_notes_update

--- application/views/admin/Travel_planner/inc/warning.php
+++ application/views/admin/Travel_planner/inc/warning.php
@@ -1,12 +1,12 @@
   <!-- SweetAlert2 -->
   <link rel="stylesheet" href="<?=base_url('assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')?>">
   <!-- jQuery -->
-      <script src="<?=base_url('assets/plugins/jquery/jquery.min.js')?>"></script>
-      <!-- Bootstrap 4 -->
-      <script src="<?=base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
-      <!-- SweetAlert2 -->
-      <script src="<?=base_url('assets/plugins/sweetalert2/sweetalert2.min.js')?>"></script>
-    <script>
+  <script src="<?=base_url('assets/plugins/jquery/jquery.min.js')?>"></script>
+  <!-- Bootstrap 4 -->
+  <script src="<?=base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+  <!-- SweetAlert2 -->
+  <script src="<?=base_url('assets/plugins/sweetalert2/sweetalert2.min.js')?>"></script>
+  <script>
     $(function() {
     var Toast = Swal.mixin({
       toast: false,
@@ -20,4 +20,4 @@
         title: '<?=$errorTitle?>!!\n<?=$error?>'
       })
     });
-</script>
+  </script>
---
+++ notes.md
@@ -0,0 +1,27 @@
+# Changes to be Made
+
+- [x] Register Page and Login Page Redirection
+- [x] Register Failed and success
+- [x] test-login Remember
+- [x] Login Failed Same Page
+- [x] Blank Profile Error Check
+- [x] Blank Profile Pic Default image
+- [x] Change Password Confirmation
+- [x] One Way Disable and Clear Value
+- [x] Travel Note Confirmation
+- [x] Travel Note Edit AutoComplete
+- [x] Export in Excel or PDF
+- [x] Ask about Selection Suggestion AJAX
+  - [x] TriedautoComplete Library JQuery
+  - [x] Done by adding Datalist in Site Response
+<!-- - [ ] Date of Birth YEAR -->
+- [ ] Add Data using Import
+  -[ ] Check if Data Type is Same
+  -[x] Check if Duplicate Entry
+  -[x] Do CSV
+
+- [ ] Ask about Edit Post Variable Returns in js Functions
+
+## NOTES
+
+- SQL Path Separator - '/'
