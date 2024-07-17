fb0a0e9401603d37a3aa08b6dd654c45518fc419
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-07-17 23:55:47 +0530

    Removed_index_php_from_URL

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://10.255.254.37/CodeIgniterApp/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
