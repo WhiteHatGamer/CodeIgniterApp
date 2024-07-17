4b3605672a10a7659261149ec248b0664157b210
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-07-18 00:05:16 +0530

    T_p_folder_structure_change_in_paths

--- application/helpers/asset_helper.php
+++ application/helpers/asset_helper.php
@@ -7,6 +7,6 @@
 
     // Helper to get assets folder from Base folder
     function Travel_plannerInc(){
-        return APPPATH.'views/admin/Travel_planner/inc/';
+        return APPPATH.'views/Travel_planner/inc/';
     }
 ?>
