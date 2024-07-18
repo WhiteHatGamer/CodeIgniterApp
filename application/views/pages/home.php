57b6c779e8dcbd848d30e9b311f1f21771090e14
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-07-18 18:35:04 +0530

    @SERVER_key_fix

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,4 +1,4 @@
-<h3><a href="<?=$_SERVER['PHP_SELF']?>/news">News</a></h3>
-<h3><a href="<?=$_SERVER['PHP_SELF']?>/admin/">Dashboard</a></h3>
-<h3><a href="<?=$_SERVER['PHP_SELF']?>/admin/Travel_planner">Travel Planner</a></h3>
-<h3><a href="<?=$_SERVER['PHP_SELF']?>/About">About Us</a></h3>
+<h3><a href="<?=$_SERVER['REQUEST_URI']?>news">News</a></h3>
+<h3><a href="<?=$_SERVER['REQUEST_URI']?>admin/">Dashboard</a></h3>
+<h3><a href="<?=$_SERVER['REQUEST_URI']?>Travel_planner">Travel Planner</a></h3>
+<h3><a href="<?=$_SERVER['REQUEST_URI']?>About">About Us</a></h3>
