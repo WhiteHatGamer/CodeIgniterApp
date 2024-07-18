55274a126d118c7d8b005c50da83f9be6854d5ad
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-07-18 17:58:38 +0530

    Removed_indexphp_from_hyperlinks_andUrls

--- application/views/admin/views/examples/404.php
+++ application/views/admin/views/examples/404.php
@@ -22,7 +22,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="<?= base_url()."index.php/admin/"?>frames/dashboard3" class="nav-link">Home</a>
+        <a href="<?= base_url()."admin/"?>frames/dashboard3" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
@@ -155,7 +155,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?= base_url()."index.php/admin/"?>frames/dashboard3" class="brand-link">
+    <a href="<?= base_url()."admin/"?>frames/dashboard3" class="brand-link">
       <img src="<?=base_url('assets/dist/img/AdminLTELogo.png')?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
@@ -211,13 +211,13 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="<?= base_url()."index.php/admin/"?>frames/dashboard2" class="nav-link">
+                <a href="<?= base_url()."admin/"?>frames/dashboard2" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Dashboard v2</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="<?= base_url()."index.php/admin/"?>frames/dashboard3" class="nav-link">
+                <a href="<?= base_url()."admin/"?>frames/dashboard3" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Dashboard v3</p>
                 </a>
@@ -699,7 +699,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="<?= base_url()."index.php/admin/"?>frames/starter" class="nav-link">
+                <a href="<?= base_url()."admin/"?>frames/starter" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Starter Page</p>
                 </a>
@@ -731,7 +731,7 @@
           </li>
           <li class="nav-header">MISCELLANEOUS</li>
           <li class="nav-item">
-            <a href="<?= base_url()."index.php/admin/"?>frames/iframe" class="nav-link">
+            <a href="<?= base_url()."admin/"?>frames/iframe" class="nav-link">
               <i class="nav-icon fas fa-ellipsis-h"></i>
               <p>Tabbed frames/iframe Plugin</p>
             </a>
@@ -862,7 +862,7 @@
 
           <p>
             We could not find the page you were looking for.
-            Meanwhile, you may <a href="<?= base_url()."index.php/admin/"?>dashboard">return to dashboard</a> or try using the search form.
+            Meanwhile, you may <a href="<?= base_url()."admin/"?>dashboard">return to dashboard</a> or try using the search form.
           </p>
 
           <form class="search-form">
