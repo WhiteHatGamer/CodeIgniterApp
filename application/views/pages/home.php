6e98c761b30d379c8b52fe547b03b3d0a3d6b1a8
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-07-19 14:35:35 +0530

    Sub_domain_error_fixed

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,4 +1,4 @@
-<h3><a href="<?=$_SERVER['REQUEST_URI']?>news">News</a></h3>
+<h3><a href="<?=$_SERVER['REQUEST_URI']?>news/">News</a></h3>
 <h3><a href="<?=$_SERVER['REQUEST_URI']?>admin/">Dashboard</a></h3>
-<h3><a href="<?=$_SERVER['REQUEST_URI']?>Travel_planner">Travel Planner</a></h3>
-<h3><a href="<?=$_SERVER['REQUEST_URI']?>About">About Us</a></h3>
+<h3><a href="<?=$_SERVER['REQUEST_URI']?>Travel_planner/">Travel Planner</a></h3>
+<h3><a href="<?=$_SERVER['REQUEST_URI']?>About/">About Us</a></h3>
