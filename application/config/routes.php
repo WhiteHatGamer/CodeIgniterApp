b0c51386b3b030a46ec39e3dfa29820484bbae80
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-07-21 16:33:03 +0530

    create404Page

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'pages';
-$route['404_override'] = '';
+$route['404_override'] = 'errors/page_missing';
 $route['translate_uri_dashes'] = FALSE;
 $route['news'] = 'news';
 $route['news/(:any)'] = 'news/view/$1';
