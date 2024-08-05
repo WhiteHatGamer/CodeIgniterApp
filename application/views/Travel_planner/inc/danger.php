da5f6bd4c3de51aa2fe9e39ac4e938b823e586ec
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-08-05 13:33:47 +0400

    dangerAlertInc

---
+++ application/views/Travel_planner/inc/danger.php
@@ -0,0 +1,24 @@
+  <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="<?=base_url('assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')?>">
+  <!-- jQuery -->
+  <script src="<?=base_url('assets/plugins/jquery/jquery.min.js')?>"></script>
+  <!-- Bootstrap 4 -->
+  <script src="<?=base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+  <!-- SweetAlert2 -->
+  <script src="<?=base_url('assets/plugins/sweetalert2/sweetalert2.min.js')?>"></script>
+  <script>
+    $(function() {
+    var Toast = Swal.mixin({
+      toast: false,
+      position: '',
+      showConfirmButton: false,
+      timer: 5000
+    });
+
+    Toast.fire({
+        icon: 'error',
+        title: '<?=$errorTitle?>!!\n<?=$error?>',
+        html: '<?php if(!empty($warningHtml)){echo $warningHtml;}?>',
+      })
+    });
+  </script>
