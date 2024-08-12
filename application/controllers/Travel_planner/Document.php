21d5de3d473b03dab91de9f9e64af45b39688385
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-08-12 11:07:24 +0400

    uploadAction

--- application/controllers/Travel_planner/Document.php
+++ application/controllers/Travel_planner/Document.php
@@ -36,11 +36,64 @@ use FontLib\Table\Type\post;
                 return;
             }
 
+            if($this->input->post('upload')){
+
+                // Upload Form  Submitted
+                $config['upload_path']      = FCPATH."assets/uploads";
+                $config['allowed_types']    = 'gif|jpg|jpeg|png|bmp|webp|tiff|x-icon';
+
+                $this->load->library('upload', $config);
+                
+                // Storing Image array as Separate
+                $count = count($_FILES['images']['name']);
+                for ($i=0;$i<$count;$i++) {
+                    $_FILES['image'.$i]['name'] = $_FILES['images']['name'][$i];
+                    $_FILES['image'.$i]['full_path'] = $_FILES['images']['full_path'][$i];
+                    $_FILES['image'.$i]['type'] = $_FILES['images']['type'][$i];
+                    $_FILES['image'.$i]['tmp_name'] = $_FILES['images']['tmp_name'][$i];
+                    $_FILES['image'.$i]['error'] = $_FILES['images']['error'][$i];
+                    $_FILES['image'.$i]['size'] = $_FILES['images']['size'][$i];
+                }
+                $source = array();
+                for ($i=0; $i<$count; $i++){
+
+                    if($this->upload->do_upload('image'.$i)){
+                        // Saving File into Database
+                        $source[$i]['path'] = $this->upload->data()['full_path'];
+                        $source[$i]['type'] = $_FILES['image'.$i]['type'];
+
+                    }
+                }
+
+                // Uploading all source File to Database and showing Toast
+                if($this->document_model->uploadImages($source)){
+                    $this->load->view("Travel_planner/inc/saved");
+                }
+                
+            }
             
             $this->load->view("Travel_planner/inc/header");
             $this->load->view("Travel_planner/Dashboard/Document/Upload");
             $this->load->view("Travel_planner/inc/footer");
         }
 
+        public function enlarge(){
+
+            // Checking If User is Logged In
+            if(!$this->session->email){
+                redirect(adminTravelPlannerUrl());
+                return;
+            }
+
+            $email = $this->session->email;
+            $id = $_GET['id'];
+            $data = @$this->document_model->getImage(array('id'=>$id, 'user_id'=>$email))[0];
+            if($data){
+
+                $this->load->view("Travel_planner/Dashboard/Document/enlarge", $data);
+            }else{
+                show_404();
+            }
+        }
     }
     
