90cb26d133da72abd7dbbfd5d6f26c87a9c0cd43
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-09-16 10:43:52 +0400

    tourismController

---
+++ application/controllers/Travel_planner/Tourism.php
@@ -0,0 +1,18 @@
+<?php
+
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    use PhpOffice\PhpSpreadsheet\Reader\Csv as CsvReader;
+
+    class Tourism extends CI_Controller{
+        
+        public function __construct(){
+                
+            parent::__construct();
+            $this->load->library("session");
+            $this->load->model('journey_model');
+            $this->load->model('hotel_model');
+            $this->load->model('city_model');
+            $this->load->model('document_model');
+        }
+
+    }
