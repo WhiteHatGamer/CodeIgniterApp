7259e861dc6f5ea122639abbd8907c6565eac1ef
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-09-17 15:45:07 +0400

    generalizedResponseCodes

--- application/controllers/Travel_planner/Api.php
+++ application/controllers/Travel_planner/Api.php
@@ -94,4 +94,95 @@
         }
         return;
     }
+
+    private function throw_response($status, $data="null"){
+        switch($status){
+            case 200:
+                // Success
+                $this->output
+                    ->set_status_header(200)
+                    ->set_content_type("application/json", "utf-8")
+                    ->set_output(
+                        json_encode([
+                        "status"=>STATUS_SUCCESS,
+                        "message"=>"Operation Completed Successfully",
+                        "data"=>$data
+                        ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+                );
+            case 400:
+                // Bad Request
+                $this->output
+                    ->set_status_header(400)
+                    ->set_content_type("application/json", "utf-8")
+                    ->set_output(
+                        json_encode([
+                        "status"=>STATUS_FAIL,
+                        "message"=>"Bad Request",
+                        "data"=>$data
+                        ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+                    );
+            case 401:
+                // Unauthorized
+                $this->output
+                    ->set_status_header(401)
+                    ->set_content_type("application/json", "utf-8")
+                    ->set_output(
+                        json_encode([
+                        "status"=>STATUS_FAIL,
+                        "message"=>"Unauthorized",
+                        "data"=>$data
+                        ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+                    );
+            case 403:
+                // Forbidden
+                $this->output
+                    ->set_status_header(403)
+                    ->set_content_type("application/json", "utf-8")
+                    ->set_output(
+                        json_encode([
+                        "status"=>STATUS_FAIL,
+                        "message"=>"Forbidden",
+                        "data"=>$data
+                        ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+                    );
+            case 404:
+                // Not Found
+                $this->output
+                    ->set_status_header(404)
+                    ->set_content_type("application/json", "utf-8")
+                    ->set_output(
+                        json_encode([
+                        "status"=>STATUS_FAIL,
+                        "message"=>"Not Found",
+                        "data"=>$data
+                        ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+                    );
+            case 500:
+                // Internal Server Error
+                $this->output
+                    ->set_status_header(500)
+                    ->set_content_type("application/json", "utf-8")
+                    ->set_output(
+                        json_encode([
+                        "status"=>STATUS_ERROR,
+                        "message"=>"Internal Server Error",
+                        "data"=>$data
+                        ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+                    );
+            default:
+                // Unknown Error
+                $this->output
+                    ->set_content_type("application/json", "utf-8")
+                    ->set_output(
+                        json_encode([
+                        "status"=>STATUS_ERROR,
+                        "message"=>"Unknown Error",
+                        "data"=>""
+                        ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+                    );
+
+            return;
+        }
+    }
+    
 }
