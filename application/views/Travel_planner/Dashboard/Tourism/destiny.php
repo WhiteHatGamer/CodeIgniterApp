918a449d038b70f33fd6247386428acc70ad560e
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2024-09-19 18:12:18 +0400

    destinationPageViews

---
+++ application/views/Travel_planner/Dashboard/Tourism/destiny.php
@@ -0,0 +1,26 @@
+<?php
+    $imgLen = count($images);
+    $destLen = count($destinations);
+    $maxLen = max($imgLen,$destLen);
+    $packageUrl = "https://www.holidaymakers.com/index.php/package/search_holidays?to=$1&adult=1&child=0"
+    ?>
+
+        
+    <div class='card card-success'>
+        <div class='card-body'>
+            <div class='row'><?php
+                for($i=0;$i<$maxLen; $i++){
+                    ?>
+                    <div class='col-3 img-thumbnail'>
+                        <div class='position-absolute m-2 rounded bg-primary' style="bottom: 0%; right: 0%; width: 96%; height: 14%; z-index: 100; border-top-right-radius: 0; border-top-left-radius: 0;">
+                            <a style="height: 100%; width: 100%; display: grid; align-items: center; justify-content: center;" href="<?=str_replace("$1",$countries[$i%$destLen],$packageUrl)?>"><?=$destinations[$i%$destLen]?></a>
+                        </div>
+                        <div class='card mb-2 d-flex justify-content-center' style="height: 100%;">
+                            <img class='card-img-top rounded img-fluid mx-auto d-block' src='<?=base64_decode($images[$i%$imgLen]["filePath"])?>'>
+                        </div>
+                    </div>
+                    <?php
+                }?>
+            </div>
+        </div>
+    </div>
